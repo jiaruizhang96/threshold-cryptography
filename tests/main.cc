990ee6dea8695ec4f8c9a2d3d24e91b6a5a4e104
jiaruizhang96 <skylerzhang52@gmail.com>
2024-11-06 20:12:37 -0500

    added test code that measures the time overhead for key split and reconstruction

--- tests/main.cc
+++ tests/main.cc
@@ -3,6 +3,7 @@
 #include <catch2/catch.hpp>
 #include <httplib.h>
 #include <string>
+#include "../server/split_shares.cc"
 
 using namespace httplib;
 
@@ -37,6 +38,35 @@ TEST_CASE("Benchmark PUT and GET key-value pair", "[put_get_benchmark]") {
             REQUIRE(res != nullptr);
             REQUIRE(res->status == 200); // Check for 200 OK status
             REQUIRE(res->body.find(value) != std::string::npos); // Check if response contains the value
+        };
+    }
+}
+
+
+
+TEST_CASE("Benchmark key splitting and reconstruction", "[key_split_reconstruct]") {
+    const int n = 5;  // number of shares
+    const int k = 3;  // threshold to reconstruct
+    int secret = 123456789;  
+    SECTION("Benchmark key splitting") {
+        BENCHMARK("Key splitting") {
+            auto coefficients = genCoefficients(k, secret);
+            auto shares = genSecretPairs(n, coefficients);
+            return shares;  
+        };
+    }
+
+    SECTION("Benchmark key reconstruction") {
+        // generate the shares to use for reconstruction
+        auto coefficients = genCoefficients(k, secret);
+        auto shares = genSecretPairs(n, coefficients);
+
+        // use the first k shares
+        std::vector<SecretPair> shares_to_recover(shares.begin(), shares.begin() + k);
+
+        BENCHMARK("Key reconstruction") {
+            long long recovered_secret = thresholdRecover(k, shares_to_recover);
+            return recovered_secret;  
         };
     }
 }
