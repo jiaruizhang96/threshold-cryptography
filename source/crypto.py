6de0cbf7166feff05a4bed292ea57b4c838c2292
Hunter Richards <hunter.a.richards@gmail.com>
2024-12-08 18:39:52 -0500

    Implemented ECIES protocol

--- source/crypto.py
+++ source/crypto.py
@@ -1,7 +1,9 @@
-import math
-import random
+import nacl.hash
+import nacl.secret
 
 from Crypto.PublicKey import ECC
+from math import prod
+from random import randint
 from typing import List, Tuple
 
 class Curve():
@@ -19,7 +21,7 @@ class Curve():
 class Polynomial():
     @staticmethod
     def random(S: int, t: int, q: int):
-        return Polynomial([S] + [random.randint(1, q - 1) for i in range(t - 1)], q)
+        return Polynomial([S] + [randint(1, q) for i in range(t - 1)], q)
 
     def __init__(self, a: List[int], q: int):
         self.a = a
@@ -41,6 +43,16 @@ def interpolate(shares: List[Tuple[int, int]], q: int):
         return shares[i][1]
 
     def ℓ(i: int):
-        return math.prod(x(j) * pow(x(j) - x(i), -1, q) for j in range(t) if j != i)
+        return prod(x(j) * pow(x(j) - x(i), -1, q) for j in range(t) if j != i)
 
     return sum(y(i) * ℓ(i) for i in range(t)) % q
+
+def derive_key(S: ECC.EccPoint):
+    ikm = S.x.to_bytes(S.size_in_bytes(), byteorder='big')
+    return nacl.hash.blake2b(ikm, digest_size=nacl.secret.SecretBox.KEY_SIZE, encoder=nacl.encoding.RawEncoder)
+
+def encrypt(M: bytes, K: bytes):
+    return nacl.secret.SecretBox(K).encrypt(M)
+
+def decrypt(C: bytes, K: bytes):
+    return nacl.secret.SecretBox(K).decrypt(C)
--- source/test_crypto.py
+++ source/test_crypto.py
@@ -1,16 +1,50 @@
-import random
-
 from crypto import *
+from random import randint
 
 def test_shamir():
+    # Curve parameters.
     E = Curve("P-256")
-    t = 3
-    n = 5
     q = E.q
 
-    S = random.randint(1, q - 1)
+    # Threshold encryption parameters.
+    t = 3
+    n = 5
+
+    # Shamir's secret sharing.
+    S = randint(1, q)
     f = Polynomial.random(S, t, q)
 
     shares = [(i, f(i)) for i in range(1, n + 1)]
 
+    # Check result is as expected.
     assert S == interpolate(shares[:t], q)
+
+def test_ecies():
+    # Curve parameters.
+    E = Curve('P-256')
+    q = E.q
+    G = E.G
+
+    # Message from Alice.
+    M = b"Hello world"
+
+    # Bob's key pair.
+    k = randint(1, q)
+    Q = k * G
+
+    # Alice's ephemeral key pair.
+    d = randint(1, q)
+    R = d * G
+
+    # Alice encrypts her message.
+    S = d * Q
+    K = derive_key(S)
+    C = encrypt(M, K)
+
+    # Bob decrypts Alice's message.
+    S = k * R
+    K = derive_key(S)
+    M = decrypt(C, K)
+
+    # Check result is as expected.
+    assert M.decode('utf-8') == "Hello world"
